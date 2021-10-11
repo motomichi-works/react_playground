8aa459d99ab4d5ad1cfcf988203e399f06b9bbde
motomichi <motomichi.motomichi@gmail.com>
2021-10-11 12:21:05 +0900

    rename src/components/riakuto-chapter09/molecules-and-atoms/UseState/ -> src/components/riakuto-chapter09/molecules-and-atoms/UseStateSample/

--- src/containers/RiakutoChapter09/organisms/Section02UseState.tsx
+++ src/containers/RiakutoChapter09/organisms/Section02UseState.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import BasicOrganism001 from 'components/common/organisms/BasicOrganism001';
-import UseState from 'components/riakuto-chapter09/molecules-and-atoms/UseState';
+import UseStateSample from 'components/riakuto-chapter09/molecules-and-atoms/UseStateSample';
 
 const Section02UseState: React.VFC = () => {
   const [count, setCount] = useState(0);
@@ -9,7 +9,7 @@ const Section02UseState: React.VFC = () => {
 
   return (
     <BasicOrganism001 sectionHeadingText="9-2. Hooksでstateを扱う">
-      <UseState count={count} increment={increment} reset={reset} />
+      <UseStateSample count={count} increment={increment} reset={reset} />
     </BasicOrganism001>
   );
 };
