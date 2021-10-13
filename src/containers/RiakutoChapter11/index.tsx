da1bb3196d4f3f44d776c9ed8edbb01061f564d2
motomichi <motomichi.motomichi@gmail.com>
2021-10-13 17:03:41 +0900

    11章の準備の続き

--- src/containers/RiakutoChapter11/index.tsx
+++ src/containers/RiakutoChapter11/index.tsx
@@ -3,7 +3,7 @@ import Section02Redux from 'containers/RiakutoChapter11/organisms/Section02Redux
 
 const RiakutoChapter11: React.VFC = () => (
   <Template001 pageHeadingText="RiakutoChapter11">
-    <Section02Redux />
+    <Section02Redux limit={60} />
   </Template001>
 );
 
--- src/containers/RiakutoChapter11/organisms/Section02Redux.tsx
+++ src/containers/RiakutoChapter11/organisms/Section02Redux.tsx
@@ -1,9 +1,19 @@
 import BasicOrganism001 from 'components/common/organisms/BasicOrganism001';
+import ReduxSample from 'components/riakuto-chapter11/molecules-and-atoms/ReduxSample';
+import useTimer from 'hooks/use-timer';
 
-const Section02Redux: React.VFC = () => (
-  <BasicOrganism001 sectionHeadingText="11-2. Reduxの使い方">
-    <div>Reduxを使ったコンテンツ</div>
-  </BasicOrganism001>
-);
+type Props = {
+  limit: number;
+};
+
+const Section02Redux: React.VFC<Props> = ({ limit }) => {
+  const [timeLeft, isPrime, reset] = useTimer(limit);
+
+  return (
+    <BasicOrganism001 sectionHeadingText="11-2. Reduxの使い方">
+      <ReduxSample timeLeft={timeLeft} reset={reset} isPrime={isPrime} />
+    </BasicOrganism001>
+  );
+};
 
 export default Section02Redux;
