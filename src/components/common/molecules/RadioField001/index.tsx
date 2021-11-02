76517e7cbc8f032a77dcf4db288969964b976385
motomichi <motomichi.motomichi@gmail.com>
2021-11-02 18:55:11 +0900

    RadioField001コンポーネントを作成

---
+++ src/components/common/molecules/RadioField001/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { RefCallBack } from 'react-hook-form';
+import styles from './index.module.scss';
+
+export type Choice = {
+  label: string;
+  value: string;
+};
+
+type EventHandler = ({
+  target,
+  type,
+}: {
+  target: HTMLInputElement;
+  type: string;
+}) => void;
+
+export type RadioFieldProps = {
+  handleBlur: EventHandler;
+  handleChange: EventHandler;
+  name: string;
+  choices: Choice[];
+  ref: RefCallBack;
+};
+
+const RadioField001 = React.forwardRef<HTMLInputElement, RadioFieldProps>(
+  (props, ref) => {
+    const { handleBlur, handleChange, name, choices } = props;
+
+    return (
+      <div className={styles.RadioField001}>
+        {choices.map((choice) => (
+          <div className={styles.Item}>
+            <input
+              id={`${choice.label}_${choice.value}`}
+              className={styles.Field}
+              type="radio"
+              name={name}
+              ref={ref}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            />
+            <label
+              className={styles.Label}
+              htmlFor={`${choice.label}_${choice.value}`}
+            >
+              {choice.label}
+            </label>
+          </div>
+        ))}
+      </div>
+    );
+  },
+);
+
+export default RadioField001;
