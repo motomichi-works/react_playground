48b264849afabe50040fa1ad8c8ec963f7ef7616
motomichi <motomichi.motomichi@gmail.com>
2022-01-20 15:47:45 +0900

    「9-3. Hooksで副作用を扱う」で学んだポイントをより詳細に記述

--- src/components/riakuto-chapter09/organisms/Section03UseEffect.tsx
+++ src/components/riakuto-chapter09/organisms/Section03UseEffect.tsx
@@ -9,7 +9,49 @@ const Section03UseEffect: React.VFC = () => {
       {
         tagName: 'div',
         content:
-          'useEffect()はdependencies arrayの要素が更新されたときだけ処理を実行する。',
+          'useEffect()が初回実行されるのは最初のレンダリングが行われて、その内容がブラウザに反映された直後です。',
+      },
+    ],
+    [
+      {
+        tagName: 'div',
+        content:
+          'useEffect()の第二引数に渡すdependencies arrayの要素が更新されたときだけ処理を実行する。',
+      },
+    ],
+    [
+      {
+        tagName: 'span',
+        content:
+          'useEffect()の第二引数に渡すdependencies arrayが空配列の場合は、第一引数に渡した処理が初回レンダリング時だけ実行されます。',
+      },
+    ],
+    [
+      {
+        tagName: 'span',
+        content:
+          'useEffect()の第二引数を渡さない場合は、第一引数に渡した処理が、レンダリングの度に毎回実行されます。',
+      },
+    ],
+    [
+      {
+        tagName: 'span',
+        content:
+          'useLayoutEffect()という、componentDidMountやComponentDidUpdateと同じタイミングで画面の更新をブロックして実行されるEffect Hook APIも用意されています。',
+      },
+    ],
+    [
+      {
+        tagName: 'span',
+        content:
+          'クラスコンポーネントのレンダリングは生き続けているインスタンスのrender()メソッドの実行です。そのため、インスタンス内に可変のメンバー変数としてpropsやstateは保持されます。',
+      },
+    ],
+    [
+      {
+        tagName: 'span',
+        content:
+          '関数コンポーネントのレンダリングとは、その関数自体が最初から最後まで実行されることです。そのため、関数内部で定義されている関数や変数も毎回の実行の度に定義し直され、実行が終われば破棄されます。つまり普通の関数コンポーネントは実行された後に何も残りませんが、関数コンポーネント内でState Hookが使用されていた場合は、レンダリング後のstateの値が関数の外に保存され、レンダリングが始まるタイミングで改めてコンポーネントに渡されます。',
       },
     ],
   ];
